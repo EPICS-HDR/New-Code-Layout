d53e0cf5dfe718119ee783153610c55837caa1e5
vathakkar <v.thakkar4011@gmail.com>
2024-03-04 12:30:22 -0500

    Changes to host

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ EMAIL_PORT = EMAIL_PORT
 SECRET_KEY = 'django-insecure-+9wcfz&b8$a30aq(9-$s&a^*#6lsvy^jb@3as4$0%c@f=g!cvb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['standingrock.azurewebsites.net','127.0.0.1']
 
@@ -122,8 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = STATIC_ROOT + '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
