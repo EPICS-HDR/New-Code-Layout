c5170d83bc18a48787aad7dfa6c8d4636fb70f2e
Klahre, Glenn Alastair <gklahre@purdue.edu>
2025-03-13 10:08:14 -0400

    working mae, mostly

--- services/backend/database/models.py
+++ services/backend/database/models.py
@@ -9,6 +9,7 @@ import matplotlib as mpl
 from sqlclasses import dictpull
 import time as t
 import email.utils as eu
+import copy
 import re
 
 #Function to get all data for a variable, returned as two lists, a time and a value.
@@ -36,19 +37,23 @@ def fill_in_the_blanks(times:list, values:list) -> list:
     #Give some gap, so that minor variations in time gap don't create unnecisary fill
     time_test = time_gap * 1.05
     new_values = list()
+    out_times = list()
 
 
     for time_point in range(len(new_times) - 1):
+
         current_time = datetime.datetime.fromisoformat(new_times[time_point])
         new_values.append(values[time_point])
+        out_times.append(current_time)
         #If there is a gap, fill it.
         while time_point < len(new_times) and ((current_time + time_test) < datetime.datetime.fromisoformat(new_times[time_point + 1])):
             new_values.append(values[time_point])
             current_time = current_time + time_gap
-            
+            out_times.append(current_time)
     new_values.append(values[len(new_times)-1])
+    out_times.append(current_time+time_gap)
 
-    return new_values
+    return new_values,out_times,time_gap
         
 
 #Function to break up the data into training and testing portions. With a set max length, nominally 21.
@@ -69,63 +74,225 @@ def trainTest(table:str, location:str, variable:str) -> tuple:
 
 
 #ARIMA Model
-def ARIMA(times: list, values:list, multipleSeasonality:bool) -> None:
+def ARIMA(times: list, values:list) -> pmd.ARIMA:
     #Figure out orders
     #Cross Validation
-    y_train = pmd.model_selection.train_test_split(train_size=365)
-    model = pmd.auto_arima(y_train, error_action='ignore', seasonal=True, m=365)
+    #y_train = pmd.model_selection.train_test_split(train_size=365)
+    #model = pmd.auto_arima(values, error_action='ignore', seasonal=True, m=365*24)
 
     #TODO: make multiple seasonality an if loop
     #Fourier modeling of 2nd sesonality
     #Ripped straight from an article, update for better readability, and personal use case
-    exog = pd.DataFrame({'date': y.index})
+    exog = pd.DataFrame({'date': times})
     exog = exog.set_index(pd.PeriodIndex(exog['date'], freq='D'))
     exog['sin365'] = np.sin(2 * np.pi * exog.index.dayofyear / 365.25)
     exog['cos365'] = np.cos(2 * np.pi * exog.index.dayofyear / 365.25)
     exog['sin365_2'] = np.sin(4 * np.pi * exog.index.dayofyear / 365.25)
     exog['cos365_2'] = np.cos(4 * np.pi * exog.index.dayofyear / 365.25)
     exog = exog.drop(columns=['date'])
-    exog_to_train = exog.iloc[:(len(y)-365)]
-    exog_to_test = exog.iloc[(len(y)-365):]
-    
-    arima_exog_model = pmd.auto_arima(y=y_train, exogenous=exog_to_train, seasonal=True, m=7)
+    #exog_to_train = exog.iloc[:(len(values)-365)]
+    #exog_to_test = exog.iloc[(len(y)-365):]
 
-    y_arima_exog_forecast = arima_exog_model.predict(n_periods=365, exogenous=exog_to_test)
+    print("We are here!")
+    start_time = datetime.datetime.now()
+    
+    arima_exog_model = pmd.auto_arima(y=values, exogenous=exog, seasonal=True, m=24)
+
+    end_time = datetime.datetime.now()
+
+    print("Arima Model End")
+    print(end_time-start_time)
+
+    return arima_exog_model
+
+    #y_arima_exog_forecast = arima_exog_model.predict(n_periods=365, exogenous=exog_to_test)
 
     #End ripped code
 
-    model.fit(values)
+    #model.fit(values)
+    
 
     #Gets the order for the MA term + Intergrated part. Integrate until the the series is largely stationary. 
-    sm.tsa.graphics.plot_acf()
+    #sm.tsa.graphics.plot_acf()
     #Gets the oder for the AR term
-    sm.tsa.graphics.plot_pacf()
+    #sm.tsa.graphics.plot_pacf()
     
-    sm.tsa.ARIMA()
+    #sm.tsa.ARIMA()
 
 
 
 #TBATS Model
-def tbats_model(values:list) -> list:
+def tbats_model(values:list) -> tb.TBATS:
     #Create variable estimator
-    estimator = tb.TBATS(seasonal_periods=(24, 24 * 365.25),n_jobs=2)
+    estimator = tb.TBATS(seasonal_periods=(24, 24 * 365.25),n_jobs=1)
     #Fit model to data
+    start_time = datetime.datetime.now()
     model = estimator.fit(values)
+    end_time = datetime.datetime.now()
+    print("TBATS Model Time")
+    print(end_time-start_time)
     
     #Model Forecast
     m_forecast = model.forecast(steps=24*365)
-    return m_forecast
+    return model
+
+def mae(model, test_data: list, time_data:list, time_delta: datetime, arima:bool) -> list:
+    print("MAE :)")
+    #TODO: Fill in
+    #Find out how far in each loop we have data for. 
+    day = datetime.timedelta(days=1) / time_delta
+    day3 = int(3 * day)
+    day7 = int(7 * day)
+    day21 = int(21 * day)
+    year = int(365 * day)
+    year_len = len(test_data) - year
+    day21_len = len(test_data) - day21
+    day7_len = len(test_data) - day7
+    day3_len = len(test_data) - day3
+
+
+
+    #Loop that iterates through updates. 
+    predict_size = 365 * day
+    test_array = np.array(test_data)
+    update_time = list()
+
+    day3_mae, day3_mse, day3_points_total, day7_mae, day7_mse, day7_points_total, day21_mae, day21_mse, day21_points_total, year_mae, year_mse, year_points_total = 0,0,0,0,0,0,0,0,0,0,0,0
+    
+    for point in range(len(test_data)):
+        if point % 10 == 0:
+            print(point)
+        predicts = model.predict(int(predict_size))
+        predicts = np.array(predicts)
+        if point < day3_len:
+            day3_array = test_array[point:point+day3] - predicts[:day3]
+            day3_mae += np.sum(np.abs(day3_array))
+            day3_mse += np.sum(np.power(day3_array,2))
+            day3_points_total += day3
+            predict_size = day3
+        if point < day7_len:
+            day7_array = test_array[point:point+day7] - predicts[:day7]
+            day7_mae += np.sum(np.abs(day7_array))
+            day7_mse += np.sum(np.power(day7_array,2))
+            day7_points_total += day7
+            predict_size = day7
+        if point < day21_len:
+            day21_array = test_array[point:point+day21] - predicts[:day21]
+            day21_mae += np.sum(np.abs(day21_array))
+            day21_mse += np.sum(np.power(day21_array,2))
+            day21_points_total += day21
+            predict_size = day21
+        if point < year_len:
+            year_array = test_array[point:point+year] - predicts[:year]
+            year_mae += np.sum(np.abs(year_array))
+            year_mse += np.sum(np.power(year_array,2))
+            year_points_total += year
+            predict_size = year
+            
+        st = datetime.datetime.now()
+        if arima:
+            exog = pd.DataFrame({'date': time_data[point:point]})
+            exog = exog.set_index(pd.PeriodIndex(exog['date'], freq='D'))
+            exog['sin365'] = np.sin(2 * np.pi * exog.index.dayofyear / 365.25)
+            exog['cos365'] = np.cos(2 * np.pi * exog.index.dayofyear / 365.25)
+            exog['sin365_2'] = np.sin(4 * np.pi * exog.index.dayofyear / 365.25)
+            exog['cos365_2'] = np.cos(4 * np.pi * exog.index.dayofyear / 365.25)
+            exog = exog.drop(columns=['date'])
+            model.update(test_data[point],exog,0)
+        else:
+            model.update(test_data[point])
+        end = datetime.datetime.now()
+        update_time.append(end-st)
+        
+
+    out = list()
+    out.append(day3_mae/day3_points_total)
+    out.append(day3_mse/day3_points_total)
+    out.append(day7_mae/day7_points_total)
+    out.append(day7_mse/day7_points_total)
+    out.append(day21_mae/day21_points_total)
+    out.append(day21_mse/day21_points_total)
+    #out.append(year_mae/year_points_total)
+    #out.append(year_mse/year_points_total)
+    out.append(np.average(np.array(update_time)))
+    return out
+
+
+
+            
+    
+        
+        
+
+
 
 
 
 dates1, values1 = dictpull('Carson', 'Average Air Temperature', '2000-01-01', '2024-01-01', 'mesonet')
-n_values = fill_in_the_blanks(dates1,values1)
+dates2, values2 = dictpull('Carson', 'Average Air Temperature', '2024-01-01', '2024-07-01', 'mesonet')
+n_values, n_times, time_gap = fill_in_the_blanks(dates1,values1)
+n_values2, n_times2, time_gap2 = fill_in_the_blanks(dates2, values2)
 
+model_list = list()
 print('Got here.')
 
-year_output = tbats_model(n_values)
-mpl.pyplot.plot(year_output)
-mpl.pyplot.show()
+#year_output = tbats_model(n_values)
+#mpl.pyplot.plot(year_output)
+#mpl.pyplot.show()
+model_list.append(ARIMA(n_times,n_values))
+
+out = mae(model_list[0],n_values2,n_times2,time_gap,True)
+
+def wrapper(variable:str, location:str, table:str) -> bool:
+    models = list()
+    mae_out = list()
+    train_times, train_values = dictpull(location, variable, '2000-01-01', '2024-01-01', table)
+    filled_train_values = fill_in_the_blanks(train_times, train_values)
+    test_times, test_values = dictpull(location, variable, '2024-01-01', '2024-07-01', table)
+    filled_test_values = fill_in_the_blanks(test_times, test_values)
+    models.append(tbats_model(filled_train_values))
+    models.append(ARIMA(filled_train_values))
+    # n = 0
+    # n_model = list
+    # for model in models:
+    #     if n == 1:
+    #         mae_out[n] = mae(model,filled_test_values, time_gap, True)
+    #     else:
+    #         mae_out[n] = mae(model, filled_test_values, time_gap, False)
+    #     n += 1
+    mae_out = [[43.4,643,5234,9467,124,4.2856,90.23,73,5],[]]
+
+    file_name = location.replace(" ", "") + variable.replace(" ", "") + ".txt"
+    with open(file_name) as f:
+        print("----------------------------------------------\n",file=f)
+        print("|         Predictive Model Analysis          |\n",file=f)
+        print("|--------------------------------------------|\n",file=f)
+        print("|         %s, %s          |\n",file=f)
+        print("|--------------------------------------------|\n",file=f)
+        print("|           |   SARIMA   |  TBATS  |   RNN   |\n",file=f)
+        print("|--------------------------------------------|\n",file=f)
+        print("| 3-DAY MAE |    %3d     |   %3d   |   %3d   |\n",file=f)
+        print("|--------------------------------------------|\n",file=f)
+        print("| 3-DAY MSE |    %3d     |   %3d   |   %3d   |\n",file=f)
+        print("|--------------------------------------------|\n",file=f)
+        print("| 7-DAY MAE |    %3d     |   %3d   |   %3d   |\n",file=f)
+        print("|--------------------------------------------|\n",file=f)
+        print("| 7-DAY MSE  |    %3d     |   %3d   |   %3d   |\n",file=f)
+        print("|--------------------------------------------|\n",file=f)
+        print("| 21-DAY MAE |    %3d     |   %3d   |   %3d   |\n",file=f)
+        print("|--------------------------------------------|\n",file=f)
+        print("| 21-DAY MSE |    %3d     |   %3d   |   %3d   |\n",file=f)
+        print("|--------------------------------------------|\n",file=f)
+        print("| YEAR MAE  |    %3d     |   %3d   |   %3d   |\n",file=f)
+        print("|--------------------------------------------|\n",file=f)
+        print("| YEAR MSE  |    %3d     |   %3d   |   %3d   |\n",file=f)
+        print("|--------------------------------------------|\n",file=f)
+        print("| CPU TIME  |    %3d     |   %3d   |   %3d   |\n",file=f)
+        print("|--------------------------------------------|\n",file=f)
+        
+
+
+
 
 #ARIMA(dates1,values1,True)
 
