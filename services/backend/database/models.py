ee343e5be7f150334284bba78baacabd4a0739c8
Klahre, Glenn Alastair <gklahre@purdue.edu>
2025-04-17 08:50:05 -0400

    Update models py

--- services/backend/database/models.py
+++ services/backend/database/models.py
@@ -4,6 +4,7 @@ import statsmodels.api as sm
 import statsmodels.tsa.arima.model as tsa
 import sktime.forecasting.arima as skta
 import sktime.datatypes as sktu
+import sklearn.model_selection as sms
 import datetime
 import random
 import numpy as np
@@ -168,7 +169,7 @@ def ARIMA(times: list, values:list) -> pmd.ARIMA:
 
 #TBATS Model
 def tbats_model(values:list) -> tb.TBATS:
-    #Create variable estimator
+    #Create variable estimator, with the two periods as an input.
     estimator = tb.TBATS(seasonal_periods=(24, 24 * 365.25),n_jobs=1)
     #Fit model to data
     start_time = datetime.datetime.now()
@@ -181,7 +182,10 @@ def tbats_model(values:list) -> tb.TBATS:
     #m_forecast = model.forecast(steps=24*365)
     return model
 
-def mae(model, train_data:list, test_data: list, time_data:list, time_delta: datetime, arima:bool, update_size:int) -> list:
+def mae(model, train_data:list, test_data: list, time_data:list, time_delta: datetime, arima:bool, rnn:bool, rnn_params:list, update_size:int) -> list:
+    min_norm = rnn_params[0]
+    norm_range = rnn_params[1]
+    rnn_input_size = rnn_params[2]
     print("MAE :)")
     #TODO: Fill in
     #Find out how far in each loop we have data for. 
@@ -194,7 +198,8 @@ def mae(model, train_data:list, test_data: list, time_data:list, time_delta: dat
     day21_len = len(test_data) - day21
     day7_len = len(test_data) - day7
     day3_len = len(test_data) - day3
-
+    if rnn:
+        times = pd.DataFrame(time_data)
 
 
     #Loop that iterates through updates. 
@@ -219,10 +224,24 @@ def mae(model, train_data:list, test_data: list, time_data:list, time_delta: dat
             if predict_size != len(exog):
                 predict_size = len(exog)
             predicts = model.forecast(int(predict_size), exog=exog)
+        elif rnn:
+            if point == 0:
+                xinput = train_data[-rnn_input_size:]
+            elif(point < rnn_input_size):
+                xinput = train_data[point-rnn_input_size:]
+                xinput.extend(test_data[:point])
+            else:
+                xinput = test_data[point-rnn_input_size:point]
+            xday = np.array(list([times[0][point].dayofyear/366]))
+            #Normalize 
+            xinput = (np.array(xinput) - min_norm) / norm_range
+            xinput = np.array(xinput).reshape((1,rnn_input_size,1)) 
+            predicts = model.predict(x=[xinput,xday])
+            predicts = (predicts[0] * norm_range) + min_norm
         else: 
             predicts = model.forecast(int(predict_size))
 
-        predicts = np.array(predicts)
+        #predicts = np.array(predicts)
         if point < day3_len:
             day3_array = test_array[point:point+day3] - predicts[:day3]
             day3_mae += np.sum(np.abs(day3_array))
@@ -264,7 +283,7 @@ def mae(model, train_data:list, test_data: list, time_data:list, time_delta: dat
             
 
             #model.forecast(predict_size,exog)
-        else:
+        elif rnn is False:
             tbats_data.extend(test_data[point:point+1+update_size])
             model.fit(tbats_data)
             #model.forecast(predict_size)
@@ -346,13 +365,17 @@ def mae(model, train_data:list, test_data: list, time_data:list, time_delta: dat
 def wrapper(location:str, variable:str, table:str) -> bool:
     models = list()
     mae_out = list()
+    #Gets the training data for the location and variable
     train_times, train_values = dictpull(location, variable, '2000-01-01', '2024-01-01', table)
+    #Fills the gaps in the data.
     filled_train_values, filled_train_times, train_time_gap = fill_in_the_blanks(train_times, train_values)
+    #Gets the test data.
     test_times, test_values = dictpull(location, variable, '2024-01-01', '2024-07-01', table)
+    #Fills the test data.
     filled_test_values, filled_test_times, test_time_gap = fill_in_the_blanks(test_times, test_values)
 
     #Create and Save TBATS Model
-    tbats_filename = 'services/backend/database/models/' + location.replace(" ","") + '_' + variable.replace(" ", "") + '_tbats_initial.pkl'
+    tbats_filename = 'services/backend/database/models/' + location.replace(" ","") + '_' + variable.replace(" ", "") + '_TBATS_initial.pkl'
     if os.path.exists(tbats_filename):
         print("Loading existing TBATS Model.")
         with open(tbats_filename, 'rb') as pkl:
@@ -380,16 +403,38 @@ def wrapper(location:str, variable:str, table:str) -> bool:
 
     n = 0
     n_model = list()
+
+    #Gets the saved RNN if it exists. Creates a new one if it doesn't.
+    rnn_filename = 'services/backend/database/models/' + location.replace(" ","") + '_' + variable.replace(" ","") + '_RNN_initial.pkl'
+    if os.path.exists(rnn_filename):
+        print("Loading existing RNN Model.")
+        with open(rnn_filename, 'rb') as pkl:
+            rnn_model = pickle.load(pkl)
+            models.append(rnn_model[0])
+    else:
+        print("Creating RNN Model. Est. Time: 30 min.")
+        rnn_model = RNN_Cr(filled_train_times,filled_train_values,train_time_gap,21)
+
+        with open(rnn_filename, 'wb') as pkl:
+            pickle.dump(rnn_model,pkl)
+        models.append(rnn_model[0])
+
+    
     for model in models:
-        if n == 1:
-            mae_out.append(mae(model,filled_train_values,filled_test_values, filled_test_times, train_time_gap, True,24*7))
+        #Perhaps the stupidest for-loop of all time. This would all be better hard coded, but I'm too lazy. Loops through all of the models and inputs them into the mae out function.
+        if n == 2:
+            mae_out.append(mae(model,filled_train_values,filled_test_values, filled_test_times, train_time_gap, False,True, rnn_model[1:4], 24*7))
+        elif n == 1:
+            mae_out.append(mae(model,filled_train_values,filled_test_values, filled_test_times, train_time_gap, True,False,rnn_model[1:4],24*7))
+            pass
         else:
-            mae_out.append(mae(model,filled_train_values,filled_test_values, filled_test_times, train_time_gap, False,24*7))
+            mae_out.append(mae(model,filled_train_values,filled_test_values, filled_test_times, train_time_gap, False,False, rnn_model[1:4], 24*7))
         n += 1
     mae_out.append([0,0,0,0,0,0,0,0,0])
     #mae_out = [[43.4,643,5234,9467,124,4.2856,90.23,73,5],[55.3,783,1023,8,126,5.67901,90.23,409,4],[0,0,0,0,0,0,0,0,0]]
 
     file_name = location.replace(" ", "") + variable.replace(" ", "") + ".txt" 
+    #Creating the output text file. 
     with open(file_name, 'w') as f:
         print("-------------------------------------------------------------\n",file=f)
         print("|                  Predictive Model Analysis                 |\n",file=f)
@@ -398,23 +443,23 @@ def wrapper(location:str, variable:str, table:str) -> bool:
         print("|------------------------------------------------------------|\n",file=f)
         print("|               |     SARIMA     |    TBATS    |     RNN     |\n",file=f)
         print("|------------------------------------------------------------|\n",file=f)
-        print(f"|   3-DAY MAE   |    {mae_out[0][0]:7}     |   {mae_out[1][0]:7}  |   {mae_out[2][0]:7}  |\n",file=f)
+        print(f"|   3-DAY MAE   |    {mae_out[0][0]:7.2}     |   {mae_out[1][0]:7.2}  |   {mae_out[2][0]:7.2}  |\n",file=f)
         print("|------------------------------------------------------------|\n",file=f)
-        print(f"|   3-DAY MSE   |    {mae_out[0][1]:7}     |   {mae_out[1][1]:7}   |   {mae_out[2][1]:7}   |\n",file=f)
+        print(f"|   3-DAY MSE   |    {mae_out[0][1]:7.2}     |   {mae_out[1][1]:7.2}   |   {mae_out[2][1]:7.2}   |\n",file=f)
         print("|------------------------------------------------------------|\n",file=f)
-        print(f"|   7-DAY MAE   |    {mae_out[0][2]:7}     |   {mae_out[1][2]:7}   |   {mae_out[2][2]:7}   |\n",file=f)
+        print(f"|   7-DAY MAE   |    {mae_out[0][2]:7.2}     |   {mae_out[1][2]:7.2}   |   {mae_out[2][2]:7.2}   |\n",file=f)
         print("|------------------------------------------------------------|\n",file=f)
-        print(f"|   7-DAY MSE    |    {mae_out[0][3]:7}     |   {mae_out[1][3]:7}   |   {mae_out[2][3]:7}   |\n",file=f)
+        print(f"|   7-DAY MSE    |    {mae_out[0][3]:7.2}     |   {mae_out[1][3]:7.2}   |   {mae_out[2][3]:7.2}   |\n",file=f)
         print("|------------------------------------------------------------|\n",file=f)
-        print(f"|   21-DAY MAE   |    {mae_out[0][4]:7}     |   {mae_out[1][4]:7}   |   {mae_out[2][4]:7}   |\n",file=f)
+        print(f"|   21-DAY MAE   |    {mae_out[0][4]:7.2}     |   {mae_out[1][4]:7.2}   |   {mae_out[2][4]:7.2}   |\n",file=f)
         print("|------------------------------------------------------------|\n",file=f)
-        print(f"|   21-DAY MSE   |    {mae_out[0][5]:7}     |   {mae_out[1][5]:7}   |   {mae_out[2][5]:7}   |\n",file=f)
+        print(f"|   21-DAY MSE   |    {mae_out[0][5]:7.2}     |   {mae_out[1][5]:7.2}   |   {mae_out[2][5]:7.2}   |\n",file=f)
         print("|------------------------------------------------------------|\n",file=f)
-        print(f"|   YEAR MAE    |    {mae_out[0][6]:7}     |   {mae_out[1][6]:7}  |   {mae_out[2][6]:7}   |\n",file=f)
+        print(f"|   YEAR MAE    |    {mae_out[0][6]:7.2}     |   {mae_out[1][6]:7.2}  |   {mae_out[2][6]:7.2}   |\n",file=f)
         print("|------------------------------------------------------------|\n",file=f)
-        print(f"|   YEAR MSE    |    {mae_out[0][7]:7}     |   {mae_out[1][7]:7}   |   {mae_out[2][7]:7}   |\n",file=f)
+        print(f"|   YEAR MSE    |    {mae_out[0][7]:7.2}     |   {mae_out[1][7]:7.2}   |   {mae_out[2][7]:7.2}   |\n",file=f)
         print("|------------------------------------------------------------|\n",file=f)
-        print(f"|   CPU TIME    |    {mae_out[0][8]}     |   {mae_out[1][8]}   |   {mae_out[2][8]}   |\n",file=f)
+        print(f"| UPDATE CPU TIME |    {mae_out[0][8]:.2}     |   {mae_out[1][8]:.2}   |   {mae_out[2][8]:.2}   |\n",file=f)
         print("|------------------------------------------------------------|\n",file=f)
         
 
@@ -457,7 +502,7 @@ def rnn_shape(time_gap: datetime.timedelta, how_far: int) -> list:
 
     return shapes
 
-def rnn_training_data(times:list, values:list, shapes:list) -> list:
+def rnn_training_data(times:list, values:list, shapes:list, data_aug:bool) -> list:
     "Create training data based on the shape of the input and output."
 
     times = pd.DataFrame(times)
@@ -465,9 +510,10 @@ def rnn_training_data(times:list, values:list, shapes:list) -> list:
 
     values = np.array(values)
     values_range = (max(values) - min(values))
-    values = (values - (min(values)+(values_range*.1)))/(values_range*1.2) 
+    min_values = min(values) - values_range*.1
+    values = (values - (min(values)-(values_range*.1)))/(values_range*1.2) 
 
-
+    sd = np.std(values)
     input_size = shapes[0][0]
     output_size = shapes[1][0]
     window_size = (shapes[0][0]) + shapes[1][0]
@@ -481,25 +527,36 @@ def rnn_training_data(times:list, values:list, shapes:list) -> list:
         input.append(values[time:time+input_size])
         day.append(times[0][time+input_size].dayofyear/ 366)
         output.append(values[time+input_size:time+window_size])
-    return (np.array(input), np.array(output), np.array(day))
+
+        #Data aug inputs
+        if data_aug:
+            input.append((np.array(values[time:time+input_size]) + np.random.normal(loc=0.0,scale=(sd/4.0),size=input_size)))
+            day.append(times[0][time+input_size].dayofyear/ 366)
+            output.append(values[time+input_size:time+window_size]+ np.random.normal(loc=0.0,scale=(sd/4.0),size=input_size))
+
+        #Data
+    
+    return (np.array(input), np.array(output), np.array(day),min_values,values_range*1.2)
 
 
 def RNN_Cr(times:list, values:list,time_gap:datetime.timedelta,how_far:int):
     shapes = rnn_shape(time_gap,how_far)
-    training_data = rnn_training_data(times,values,shapes)
+    training_data = rnn_training_data(times,values,shapes,True)
 
     #model = keras.models.Sequential()
     print(shapes[0])
+    #Input layers
     inputRNN = keras.Input(shape=shapes[0])
     inputDense = keras.Input(shape=shapes[2])
-
+    #RNN Upper layers
     rnn = keras.layers.SimpleRNN(shapes[1][0],activation='linear')(inputRNN)
     rnn = keras.Model(inputs=inputRNN,outputs=rnn)
     inputDense = keras.Model(inputs=inputDense,outputs=inputDense)
 
+    #Combining in the new input(Day of the Year) with the RNN output. 
     combinedInput = keras.layers.concatenate([rnn.output,inputDense.output])
 
-
+    #Dense Lower Layers
     dense = keras.layers.Dense(4096)(combinedInput)
     dense = keras.layers.Dense(1024)(dense)
     dense = keras.layers.Dense(shapes[1][0])(dense)
@@ -512,16 +569,105 @@ def RNN_Cr(times:list, values:list,time_gap:datetime.timedelta,how_far:int):
     # #model = keras.layers.SimpleRNN(shapes=[1][0],activation='linear')(model)
     # outputs = keras.layers.Dense(shapes[1][0])
     # model = keras.Model(inputs=inputs,outputs=outputs,name="first_model")
+
+    #Visual outputs
     model.summary()
     plot_model(model,'model.png',show_shapes=True,show_layer_activations=True)
+
+    #Make and fit the model
     model.compile(loss='mean_squared_error', optimizer='adam')
-
     model.fit(x=[training_data[0], training_data[2]], y=training_data[1], epochs=5)
-
+    
+    #Save RNN
     with open('RNN.pkl','wb') as pkl:
         pickle.dump(model, pkl)
     
-    return model
+    return model, training_data[3], training_data[4], shapes[0][0]
+
+
+def data_retrieve(location:str,variable:str,table:str,date:str=None)-> tuple:
+    "Retrieves data for a variable."
+    try:
+        if date is None:
+            date = datetime.datetime.today().isoformat()
+        
+        times, values = dictpull(location,variable,'2000-01-01',date,table)
+    except:
+        print("Could not find data.")
+        raise RetrieveError 
+    else:
+        return times, values
+
+def model_retrieve(location:str,variable:str,model_type:str) -> Model:
+    """Retrieves saved model. model_type should be :'RNN', 'ARIMA', or 'TBATS'"""
+    filename = 'services/backend/database/models/' + location.replace(" ","") + '_' + variable.replace(" ","") + '_' + model_type + '_initial.pkl'
+    try:
+        with open(filename, 'rb') as pkl:
+            model = pickle.load(pkl)
+    except:
+        print("Could not retrieve mode.")
+        raise RetrieveError
+    else:
+        return model
+
+        
+def model_display(location:str,variable:str,table:str,model_type:str) ->None:
+    output_size = 504
+    data = data_retrieve(location,variable,table)
+    model = model_retrieve(location,variable,model_type)
+
+    #Make times a pandas dataframe so dayofyear property can be used.
+    times = pd.DataFrame(data[1])
+
+    if model_type == 'RNN':
+        #Renaming upon unpacking for ease of use
+        rnn_params = model[1:4]
+        model = model[0]
+        min_norm = rnn_params[0]
+        norm_range = rnn_params[1]
+        rnn_input_size = rnn_params[2]
+        #Cut data for input 
+        xinput = data[0][-output_size-rnn_input_size:-output_size]
+        xday = np.array(list([times[-output_size].dayofyear/366]))
+        #Normalize and reshape for input.
+        xinput = (np.array(xinput) - min_norm) / norm_range
+        xinput = np.array(xinput).reshape((1,rnn_input_size,1)) 
+        #Predict future data points
+        predicts = model.predict(x=[xinput,xday])
+        #Unnormalize 
+        predicts = (predicts[0] * norm_range) + min_norm
+
+        pass
+    elif model_type == 'ARIMA':
+        #Create exogenous data neccisary for future predictions
+        exog = pd.DataFrame({'date': data[1][]})
+        exog = exog.set_index(pd.PeriodIndex(exog['date'], freq='H'))
+        exog['sin365'] = np.sin(2 * np.pi * exog.index.dayofyear / 365.25)
+        exog['cos365'] = np.cos(2 * np.pi * exog.index.dayofyear / 365.25)
+        exog['sin365_2'] = np.sin(4 * np.pi * exog.index.dayofyear / 365.25)
+        exog['cos365_2'] = np.cos(4 * np.pi * exog.index.dayofyear / 365.25)
+        exog = exog.drop(columns=['date'])
+        #Refit the model without changing parameters
+        model = model.apply(data[0][:-output_size],exog[:-output_size])
+        #Predict
+        predicts = model.predict(output_size,exog[-output_size:])
+        pass
+
+    elif model_type == 'TBATS':
+        tbats_data = data[0][:-output_size]
+        model.fit(tbats_data)
+        predicts = model.forcast(output_size)
+        pass
+    else:
+        print("Bad model type.")
+
+    #Plot the prediction vs. the actual data.
+    fig = mpl.pyplot.plot(data[1][-output_size:],predicts,data[0][-output_size])
+    fig.set_xlabel("Time")
+    fig.set_ylable(variable)
+    fig.show()
+
+
 
     
 
@@ -536,27 +682,29 @@ def RNN_Cr(times:list, values:list,time_gap:datetime.timedelta,how_far:int):
 #     p.rlimit(psutil.RLIMIT_AS, (soft, hard))
 
 # memory_limit()
-#wrapper('Carson', 'Average Air Temperature','Mesonet')
+wrapper('Carson', 'Average Air Temperature','Mesonet')
+
+
 
 
 
     #TEST RNN SHAPE
-dates1, values1 = dictpull('Carson', 'Average Air Temperature', '2000-01-01', '2024-01-01', 'mesonet')
-dates2, values2 = dictpull('Carson', 'Average Air Temperature', '2024-01-01', '2024-07-01', 'mesonet')
-n_values, n_times, time_gap = fill_in_the_blanks(dates1,values1)
-n_values2, n_times2, time_gap2 = fill_in_the_blanks(dates2, values2)
+#dates1, values1 = dictpull('Carson', 'Average Air Temperature', '2000-01-01', '2024-01-01', 'mesonet')
+#dates2, values2 = dictpull('Carson', 'Average Air Temperature', '2024-01-01', '2024-07-01', 'mesonet')
+#n_values, n_times, time_gap = fill_in_the_blanks(dates1,values1)
+#n_values2, n_times2, time_gap2 = fill_in_the_blanks(dates2, values2)
 
-shapes = rnn_shape(time_gap,21)
+#shapes = rnn_shape(time_gap,21)
 
 
 
     #TEST RNN TRAINING VALUES
-training_data = rnn_training_data(n_times,n_values,shapes=shapes)
+#training_data = rnn_training_data(n_times,n_values,shapes=shapes,data_aug=True)
 #print(len(training_data[0][0]))
 #print(len(training_data[0][1]))
 
     #TEST RNN
-RNN_Cr(n_times,n_values,time_gap,21)
+#RNN_Cr(n_times,n_values,time_gap,21)
     
     
 
