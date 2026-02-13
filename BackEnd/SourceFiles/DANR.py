7e073e35206e568307cdb509c46b3629c59aeab6
Nj38 <jairamkartik38@gmail.com>
2026-02-12 20:01:37 -0500

    updated DANR
    More elegant solution for removing duplicate data.

--- BackEnd/SourceFiles/DANR.py
+++ BackEnd/SourceFiles/DANR.py
@@ -1,11 +1,9 @@
 import requests
-from datetime import datetime
 import pandas as pd
 import sqlite3
+import sqlite_utils
 import traceback
 from config import DANRConfig
-
-
 
 def _pull(debug=False):
     data = []
@@ -13,51 +11,30 @@ def _pull(debug=False):
         try:
             searchTerm = DANRConfig['baseURL'] + station
             data.append(requests.get(searchTerm).json())
-
         except Exception as e:
             print(type(e).__name__+ ", skipping " + station)
             if debug:
                 traceback.print_exc()
-
     return data
-
     
 def _process(data):
-    format_string = DANRConfig['dateTimeFormat']
-    count = 0
-    for station_index, station in enumerate(data):
-        for sample in station['parameters']:           
-            EpochTime = datetime.strptime(sample['sampleDate'], format_string)
-            if EpochTime<=self.cutoff:
-                count +=1
-
-
-    del self.data[station_index]['parameters'][:count]
-    
-def _push(data):
     conn = sqlite3.connect('mydatabase.db')
     db = pd.DataFrame()
-    for data in self.data:
-
-        meta = [['station', key] for key in list(data['station'].keys())]
-        temp = pd.json_normalize(data=data, record_path= 'parameters', meta=meta)
+    for dataPoint in data:
+        meta = [['station', key] for key in list(dataPoint['station'].keys())]
+        temp = pd.json_normalize(data=dataPoint, record_path= 'parameters', meta=meta)
         db = pd.concat([db, temp])
-
-    db.to_sql('mydatabase.db', conn, if_exists='replace', index=False)
-
-    print(db)
+    db.to_sql('temp_staging', conn, if_exists='replace', index=False)
     conn.close()
+
+def _push():
+    files = sqlite_utils.Database('mydatabase.db')
+    files["DANR"].upsert_all(files["temp_staging"].rows, alter=True, hash_id="unique_id")
 
 def update():
     data = _pull()
-    data = _process(data)
-    _push(data)
-
-
-def main():
-    data = _pull(True)
-    print(data)
+    _process(data)
+    _push()
 
 if (__name__ == "__main__"):
-    main()
-    
+    update()
